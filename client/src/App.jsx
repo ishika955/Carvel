2d6b2db9cb7b6f28a0fb84c15d67a694e6f47c4e
IShika <ishikavaidya55@gmail.com>
2026-05-20 17:25:39 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -8,20 +7,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
-}
-
-function Index() {
-  useEffect(() => {
-    window.location.href = "https://carvel.vercel.app/index.html"
-  }, []);
-  return null;
 }
 
 export default App;
