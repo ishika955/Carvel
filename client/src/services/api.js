395ff4b8e4f1151b927c7a29a73ea6f7093c08a0
Ishita kalra <ishita1244.becse24@chitkara.edu.in>
2026-05-20 13:56:53 +0530

    feat: add registerUser, updateProfilePic, getMe functions

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
   withCredentials: true
 });
 
-// Har request mein token automatically attach ho
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -13,5 +12,33 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// Register with optional profile pic
+export const registerUser = async (formData) => {
+  const res = await api.post("/api/auth/register", formData, {
+    headers: { "Content-Type": "multipart/form-data" }
+  });
+  return res.data;
+};
+
+// Login
+export const loginUser = async (data) => {
+  const res = await api.post("/api/auth/login", data);
+  return res.data;
+};
+
+// Update profile pic
+export const updateProfilePic = async (formData) => {
+  const res = await api.post("/api/upload", formData, {
+    headers: { "Content-Type": "multipart/form-data" }
+  });
+  return res.data;
+};
+
+// Get current user
+export const getMe = async () => {
+  const res = await api.get("/api/auth/me");
+  return res.data;
+};
 
 export default api;
