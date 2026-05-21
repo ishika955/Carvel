4b0a5c9fd0e5c3ac7d91f92a301e906a1326da9b
IShika <ishikavaidya55@gmail.com>
2026-05-21 12:59:15 +0530

    update sign in with google

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 function Login() {
   useEffect(() => {
-    window.location.href = "https://carvel.onrender.com/login.html";
+    window.location.href = "https://carvel.vercel.app/login.html";;
   }, []);
   return null;
 }
