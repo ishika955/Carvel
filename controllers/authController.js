86e638db06cd2c9e3522d945631d504d0443c68f
IShika <ishikavaidya55@gmail.com>
2026-05-21 12:35:38 +0530

    update sign in with google

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,6 +89,6 @@ exports.googleCallback = (req, res) => {
   );
 
 res.redirect(
-    `https://carvel.vercel.app/dashboard.html?token=${token}&role=${req.user.role}&username=${encodeURIComponent(req.user.username)}`
-  );
+  `https://carvel.vercel.app/dashboard?token=${token}&role=${req.user.role}&username=${encodeURIComponent(req.user.username)}`
+);
 };
